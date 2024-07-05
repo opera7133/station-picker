289584925e1b163b5e606d6563d9fb96a6baf53c
wamo <ainznino@pm.me>
2024-07-05 23:45:24 +0900

    keio

---
+++ assets/kanto.js
@@ -0,0 +1,554 @@
+const kantoRailways = {
+  "jr-east": {
+    id: "jr-east",
+    name: "JR東日本",
+    railway: [
+      {
+        name: "山手線",
+        id: "yamanote",
+        icon: "jr-east/jy",
+      },
+      {
+        name: "京浜東北線",
+        id: "keihin-tohoku",
+        icon: "jr-east/jk",
+      },
+      {
+        name: "東海道線",
+        id: "tokaido",
+        icon: "jr-east/jo",
+      },
+      {
+        name: "中央線快速",
+        id: "chuo-rapid",
+        icon: "jr-east/jc",
+      },
+      {
+        name: "五日市線",
+        id: "itsukaichi",
+        icon: "jr-east/jc",
+      },
+      {
+        name: "青梅線",
+        id: "oume",
+        icon: "jr-east/jc",
+      },
+      {
+        name: "中央・総武各駅停車",
+        id: "soubu",
+        icon: "jr-east/jb",
+      },
+      {
+        name: "総武快速線",
+        id: "soubu-rapid",
+        icon: "jr-east/jo",
+      },
+      {
+        name: "京葉線",
+        id: "keiyo",
+        icon: "jr-east/je",
+      },
+      {
+        name: "武蔵野線",
+        id: "musashino",
+        icon: "jr-east/jm",
+      },
+      {
+        name: "鶴見線",
+        id: "tsurumi",
+        icon: "jr-east/ji",
+      },
+      {
+        name: "鶴見線（大川支線）",
+        id: "tsurumi-okawa",
+        icon: "jr-east/ji",
+      },
+      {
+        name: "鶴見線（海芝浦支線）",
+        id: "tsurumi-umi-shibaura",
+        icon: "jr-east/ji",
+      },
+      {
+        name: "中央本線",
+        id: "chuo-honsen",
+        icon: "jr-east/chuo-honsen",
+      },
+      {
+        name: "八高線",
+        id: "hachiko",
+        icon: "jr-east/hachiko",
+      },
+      {
+        name: "総武本線",
+        id: "soubu-honsen",
+        icon: "jr-east/soubu-honsen",
+      },
+      {
+        name: "成田線",
+        id: "narita",
+        icon: "jr-east/narita",
+      },
+      {
+        name: "成田線（空港支線）",
+        id: "narita-kuko",
+        icon: "jr-east/narita",
+      },
+      {
+        name: "成田線（我孫子支線）",
+        id: "narita-abiko",
+        icon: "jr-east/narita",
+      },
+      {
+        name: "内房線",
+        id: "uchibo",
+        icon: "jr-east/uchibo",
+      },
+      {
+        name: "東金線",
+        id: "togane",
+        icon: "jr-east/togane",
+      },
+      {
+        name: "外房線",
+        id: "sotobo",
+        icon: "jr-east/sotobo",
+      },
+      {
+        name: "久留里線",
+        id: "kururi",
+        icon: "jr-east/kururi",
+      },
+      {
+        name: "鹿島線",
+        id: "kashima",
+        icon: "jr-east/kashima",
+      },
+    ],
+  },
+  "tokyo-metro": {
+    id: "tokyo-metro",
+    name: "東京メトロ",
+    railway: [
+      {
+        name: "銀座線",
+        id: "ginza",
+        icon: "tokyo-metro/g",
+      },
+      {
+        name: "丸ノ内線",
+        id: "marunouchi",
+        icon: "tokyo-metro/m",
+      },
+      {
+        name: "丸ノ内分岐線",
+        id: "marunouchi-bunki",
+        icon: "tokyo-metro/mb",
+      },
+      {
+        name: "日比谷線",
+        id: "hibiya",
+        icon: "tokyo-metro/h",
+      },
+      {
+        name: "東西線",
+        id: "tozai",
+        icon: "tokyo-metro/t",
+      },
+      {
+        name: "千代田線",
+        id: "chiyoda",
+        icon: "tokyo-metro/c",
+      },
+      {
+        name: "有楽町線",
+        id: "yurakucho",
+        icon: "tokyo-metro/y",
+      },
+      {
+        name: "半蔵門線",
+        id: "hanzomon",
+        icon: "tokyo-metro/z",
+      },
+      {
+        name: "南北線",
+        id: "namboku",
+        icon: "tokyo-metro/n",
+      },
+      {
+        name: "副都心線",
+        id: "fukutoshin",
+        icon: "tokyo-metro/f",
+      },
+    ],
+  },
+  toei: {
+    id: "toei",
+    name: "都営地下鉄",
+    railway: [
+      {
+        name: "浅草線",
+        id: "asakusa",
+        icon: "toei/a",
+      },
+      {
+        name: "三田線",
+        id: "mita",
+        icon: "toei/i",
+      },
+      {
+        name: "新宿線",
+        id: "shinjuku",
+        icon: "toei/s",
+      },
+      {
+        name: "大江戸線",
+        id: "ooedo",
+        icon: "toei/e",
+      },
+    ],
+  },
+  nt: {
+    id: "nt",
+    name: "日暮里・舎人ライナー",
+    railway: [
+      {
+        name: "日暮里・舎人ライナー",
+        id: "nt",
+        icon: "nt/nt",
+      },
+    ],
+  },
+  "tokyo-sakura": {
+    id: "tokyo-sakura",
+    name: "東京さくらトラム",
+    railway: [
+      {
+        name: "東京さくらトラム",
+        id: "tokyo-sakura",
+        icon: "tokyo-sakura/sa",
+      },
+    ],
+  },
+  keio: {
+    id: "keio",
+    name: "京王電鉄",
+    railway: [
+      {
+        name: "京王井の頭線",
+        id: "inokashira",
+        icon: "keio/in",
+      },
+      {
+        name: "京王線",
+        id: "line",
+        icon: "keio/ko",
+      },
+      {
+        name: "京王新線",
+        id: "new",
+        icon: "keio/ko",
+      },
+      {
+        name: "京王相模原線",
+        id: "sagamihara",
+        icon: "keio/ko",
+      },
+      {
+        name: "京王競馬場線",
+        id: "keibajo",
+        icon: "keio/ko",
+      },
+      {
+        name: "京王動物園線",
+        id: "doubutsuen",
+        icon: "keio/ko",
+      },
+      {
+        name: "京王高尾線",
+        id: "takao",
+        icon: "keio/ko",
+      },
+    ],
+  },
+  rinkai: {
+    id: "rinkai",
+    name: "りんかい線",
+    railway: [
+      {
+        name: "りんかい線",
+        id: "rinkai",
+        icon: "rinkai/rinkai",
+      },
+    ],
+  },
+  "tokyo-monorail": {
+    id: "tokyo-monorail",
+    name: "東京モノレール",
+    railway: [
+      {
+        name: "東京モノレール",
+        id: "tokyo-monorail",
+        icon: "tokyo-monorail/mo",
+      },
+    ],
+  },
+  yurikamome: {
+    id: "yurikamome",
+    name: "ゆりかもめ",
+    railway: [
+      {
+        name: "ゆりかもめ",
+        id: "yurikamome",
+        icon: "yurikamome/u",
+      },
+    ],
+  },
+  "tama-monorail": {
+    id: "tama-monorail",
+    name: "多摩都市モノレール",
+    railway: [
+      {
+        name: "多摩都市モノレール",
+        id: "tama-monorail",
+        icon: "tama-monorail/tt",
+      },
+    ],
+  },
+  ks: {
+    id: "ks",
+    name: "京成電鉄",
+    railway: [
+      {
+        name: "成田スカイアクセス線",
+        id: "narita-skyaccess",
+        icon: "keisei/ks_narita",
+      },
+      {
+        name: "新京成線",
+        id: "shin-keisei",
+        icon: "keisei/sl",
+      },
+      {
+        name: "本線",
+        id: "honsen",
+        icon: "keisei/ks",
+      },
+      {
+        name: "押上線",
+        id: "oshiage",
+        icon: "keisei/ks",
+      },
+      {
+        name: "金町線",
+        id: "kanamachi",
+        icon: "keisei/ks",
+      },
+      {
+        name: "千葉線",
+        id: "chiba",
+        icon: "keisei/ks",
+      },
+      {
+        name: "千原線",
+        id: "chihara",
+        icon: "keisei/ks",
+      },
+      {
+        name: "東成田線",
+        id: "higashi-narita",
+        icon: "keisei/ks",
+      },
+      {
+        name: "芝山鉄道線",
+        id: "shibayama",
+        icon: "keisei/sr",
+      },
+    ],
+  },
+  cm: {
+    id: "cm",
+    name: "千葉都市モノレール",
+    railway: [
+      {
+        name: "1号線",
+        id: "line1",
+        icon: "cm/cm_",
+      },
+      {
+        name: "2号線",
+        id: "line2",
+        icon: "cm/cm_",
+      },
+    ],
+  },
+  tb: {
+    id: "tb",
+    name: "東武鉄道",
+    railway: [
+      {
+        name: "アーバンパークライン",
+        id: "noda",
+        icon: "tobu/td",
+      },
+      {
+        name: "東上線",
+        id: "tojo",
+        icon: "tobu/tj",
+      },
+    ],
+  },
+  hs: {
+    id: "hs",
+    name: "北総鉄道",
+    railway: [
+      {
+        name: "北総線",
+        id: "hs",
+        icon: "hokso/hs",
+      },
+    ],
+  },
+  km: {
+    id: "km",
+    name: "小湊鉄道",
+    railway: [
+      {
+        name: "小湊鉄道線",
+        id: "km",
+        icon: "kominato/kominato",
+      },
+    ],
+  },
+  "yu-kari": {
+    id: "yu-kari",
+    name: "ユーカリが丘線",
+    railway: [
+      {
+        name: "ユーカリが丘線",
+        id: "yu-kari",
+        icon: "yu-kari/square",
+      },
+    ],
+  },
+  tr: {
+    id: "tr",
+    name: "東葉高速鉄道",
+    railway: [
+      {
+        name: "東葉高速線",
+        id: "tr",
+        icon: "toyo/tr",
+      },
+    ],
+  },
+  is: {
+    id: "is",
+    name: "いすみ鉄道",
+    railway: [
+      {
+        name: "いすみ線",
+        id: "is",
+        icon: "isumi/isumi",
+      },
+    ],
+  },
+  cd: {
+    id: "cd",
+    name: "銚子電気鉄道",
+    railway: [
+      {
+        name: "銚子電気鉄道線",
+        id: "cd",
+        icon: "cd/cd",
+      },
+    ],
+  },
+  ryutetsu: {
+    id: "ryutetsu",
+    name: "流鉄流山線",
+    railway: [
+      {
+        name: "流鉄流山線",
+        id: "ryutetsu",
+        icon: "ryutetsu/rn",
+      },
+    ],
+  },
+  tx: {
+    id: "tx",
+    name: "つくばエクスプレス",
+    railway: [
+      {
+        name: "つくばエクスプレス",
+        id: "tx",
+        icon: "tx/tx",
+      },
+    ],
+  },
+  kantetsu: {
+    id: "kantetsu",
+    name: "関東鉄道",
+    railway: [
+      {
+        name: "常総線",
+        id: "joso",
+        icon: "kantetsu/kantetsu",
+      },
+      {
+        name: "竜ヶ崎線",
+        id: "ryugasaki",
+        icon: "kantetsu/kantetsu",
+      },
+    ],
+  },
+  "kashima-rinkai": {
+    id: "kashima-rinkai",
+    name: "鹿島臨海鉄道大洗鹿島線",
+    railway: [
+      {
+        name: "大洗鹿島線",
+        id: "kashima-rinkai",
+        icon: "kashima-rinkai/oarai",
+      },
+    ],
+  },
+  hitachinaka: {
+    id: "hitachinaka",
+    name: "ひたちなか海浜鉄道湊線",
+    railway: [
+      {
+        name: "ひたちなか海浜鉄道湊線",
+        id: "hitachinaka",
+        icon: "hitachinaka/hitachinaka",
+      },
+    ],
+  },
+};
+
+const flattenKantoRailways = Object.values(kantoRailways).flatMap((railway) => {
+  if (railway.railway.length === 1 && railway.id !== "tb") {
+    return railway.railway.map((station) => ({
+      id: station.id,
+      name: station.name,
+    }));
+  } else {
+    return railway.railway.map((station) => ({
+      id: `${railway.id}-${station.id}`,
+      name: `${railway.name}-${station.name}`,
+    }));
+  }
+});
+
+Object.keys(kantoRailways).forEach((key) => {
+  const railway = kantoRailways[key];
+  if (railway.railway.length !== 1 || railway.id === "tb") {
+    const railwayElement = document.getElementById(key);
+    let railwayElements = "";
+    railwayElements += '<div class="ml-4 text-lg">';
+    railway.railway.forEach((station) => {
+      railwayElements += railwayTemplate(station, key);
+    });
+    railwayElements += "</div>";
+    railwayElement.innerHTML += railwayElements;
+  }
+});
---
+++ assets/keio/keio.js
@@ -0,0 +1,322 @@
+const keioStations = {
+  "keio-line": [
+    {
+      name: "新宿",
+      id: "shinjuku",
+    },
+    {
+      name: "笹塚",
+      id: "sasazuka",
+    },
+    {
+      name: "代田橋",
+      id: "daitabashi",
+    },
+    {
+      name: "明大前",
+      id: "meidaimae",
+    },
+    {
+      name: "下高井戸",
+      id: "shimotakaido",
+    },
+    {
+      name: "桜上水",
+      id: "sakura-jousui",
+    },
+    {
+      name: "上北沢",
+      id: "kamikitazawa",
+    },
+    {
+      name: "八幡山",
+      id: "hachiman-yama",
+    },
+    {
+      name: "芦花公園",
+      id: "roka-koen",
+    },
+    {
+      name: "千歳烏山",
+      id: "chitose-karasuyama",
+    },
+    {
+      name: "仙川",
+      id: "sengawa",
+    },
+    {
+      name: "つつじヶ丘",
+      id: "tsutsujigaoka",
+    },
+    {
+      name: "柴崎",
+      id: "shibasaki",
+    },
+    {
+      name: "国領",
+      id: "kokuryo",
+    },
+    {
+      name: "布田",
+      id: "fuda",
+    },
+    {
+      name: "調布",
+      id: "chofu",
+    },
+    {
+      name: "西調布",
+      id: "nishi-chofu",
+    },
+    {
+      name: "飛田給",
+      id: "tobitakyu",
+    },
+    {
+      name: "武蔵野台",
+      id: "musashinodai",
+    },
+    {
+      name: "多磨霊園",
+      id: "tama-reien",
+    },
+    {
+      name: "東府中",
+      id: "higashi-fuchu",
+    },
+    {
+      name: "府中",
+      id: "fuchu",
+    },
+    {
+      name: "分倍河原",
+      id: "bubai-gawara",
+    },
+    {
+      name: "中河原",
+      id: "naka-gawara",
+    },
+    {
+      name: "聖蹟桜ヶ丘",
+      id: "seiseki-sakuragaoka",
+    },
+    {
+      name: "百草園",
+      id: "mogusaen",
+    },
+    {
+      name: "高幡不動",
+      id: "takahata-fudo",
+    },
+    {
+      name: "南平",
+      id: "minamidaira",
+    },
+    {
+      name: "平山城址公園",
+      id: "hirayamajoshi-koen",
+    },
+    {
+      name: "長沼",
+      id: "naganuma",
+    },
+    {
+      name: "北野",
+      id: "kitano",
+    },
+    {
+      name: "京王八王子",
+      id: "keio-hachioji",
+    },
+  ],
+  "keio-new": [
+    {
+      name: "新宿",
+      id: "shinjuku",
+    },
+    {
+      name: "初台",
+      id: "hatsudai",
+    },
+    {
+      name: "幡ヶ谷",
+      id: "hatagaya",
+    },
+    {
+      name: "笹塚",
+      id: "sasazuka",
+    },
+  ],
+  "keio-inokashira": [
+    {
+      name: "渋谷",
+      id: "shibuya",
+    },
+    {
+      name: "神泉",
+      id: "shinsen",
+    },
+    {
+      name: "駒場東大前",
+      id: "komaba-todaimae",
+    },
+    {
+      name: "池ノ上",
+      id: "ike-no-ue",
+    },
+    {
+      name: "下北沢",
+      id: "shimokitazawa",
+    },
+    {
+      name: "新代田",
+      id: "shindaita",
+    },
+    {
+      name: "東松原",
+      id: "higashi-matsubara",
+    },
+    {
+      name: "明大前",
+      id: "meidaimae",
+    },
+    {
+      name: "永福町",
+      id: "eifukucho",
+    },
+    {
+      name: "西永福",
+      id: "nishi-eifuku",
+    },
+    {
+      name: "浜田山",
+      id: "hamadayama",
+    },
+    {
+      name: "高井戸",
+      id: "takaido",
+    },
+    {
+      name: "富士見ヶ丘",
+      id: "fujimigaoka",
+    },
+    {
+      name: "久我山",
+      id: "kugayama",
+    },
+    {
+      name: "三鷹台",
+      id: "mitaka-dai",
+    },
+    {
+      name: "井の頭公園",
+      id: "inokashira-koen",
+    },
+    {
+      name: "吉祥寺",
+      id: "kichijoji",
+    },
+  ],
+  "keio-sagamihara": [
+    {
+      name: "調布",
+      id: "chofu",
+    },
+    {
+      name: "京王多摩川",
+      id: "keio-tamagawa",
+    },
+    {
+      name: "京王稲田堤",
+      id: "keio-inadazutsumi",
+    },
+    {
+      name: "京王よみうりランド",
+      id: "keio-yomiuriland",
+    },
+    {
+      name: "稲城",
+      id: "inagi",
+    },
+    {
+      name: "若葉台",
+      id: "wakabadai",
+    },
+    {
+      name: "京王永山",
+      id: "keio-nagayama",
+    },
+    {
+      name: "京王多摩センター",
+      id: "keio-tama-center",
+    },
+    {
+      name: "京王堀之内",
+      id: "keio-horinouchi",
+    },
+    {
+      name: "南大沢",
+      id: "minami-oosawa",
+    },
+    {
+      name: "多摩境",
+      id: "tamasakai",
+    },
+    {
+      name: "橋本",
+      id: "hashimoto",
+    },
+  ],
+  "keio-keibajo": [
+    {
+      name: "東府中",
+      id: "higashi-fuchu",
+    },
+    {
+      name: "府中競馬正門前",
+      id: "fuchu-keiba-seimonmae",
+    },
+  ],
+  "keio-doubutsuen": [
+    {
+      name: "高幡不動",
+      id: "takahata-fudo",
+    },
+    {
+      name: "多摩動物公園",
+      id: "tama-dobutsu-koen",
+    },
+  ],
+  "keio-takao": [
+    {
+      name: "北野",
+      id: "kitano",
+    },
+    {
+      name: "京王片倉",
+      id: "keio-katakura",
+    },
+    {
+      name: "山田",
+      id: "yamada",
+    },
+    {
+      name: "めじろ台",
+      id: "mejiro-dai",
+    },
+    {
+      name: "狭間",
+      id: "hazama",
+    },
+    {
+      name: "高尾",
+      id: "takao",
+    },
+    {
+      name: "高尾山口",
+      id: "takaosan-guchi",
+    },
+  ],
+};
+
+generateRailwayStations("keio", keioStations);
