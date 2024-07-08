7b00c84c31b3457d5b355d1e012cbc895be65384
wamo <ainznino@pm.me>
2024-07-08 12:48:59 +0900

    v118

--- main.js
+++ main.js
@@ -234,76 +234,23 @@ const generateRailwayStations = (railway, railwayStations) => {
   setToggle(railway);
 };
 
-const kantoScripts = [
-  "kanto",
-  "jr-east/tokyo",
-  "jr-east/chiba",
-  "jr-east/saitama",
-  "jr-east/ibaraki",
-  "jr-east/kanagawa",
-  "tokyo-metro/tokyo-metro",
-  "tobu/chiba",
-  "tobu/saitama",
-  "seibu/seibu",
-  "keisei/ks",
-  "tx/tx",
-  "toei/toei",
-  "toei/nt",
-  "toei/sakura",
-  "keio/keio",
-  "rinkai/rinkai",
-  "tokyo-monorail/tokyo-monorail",
-  "yurikamome/yurikamome",
-  "tama-monorail/tama-monorail",
-  "hokso/hs",
-  "cm/cm",
-  "yu-kari/yu-kari",
-  "kominato/kominato",
-  "isumi/isumi",
-  "toyo/toyo",
-  "choshi/choshi",
-  "ryutetsu/ryutetsu",
-  "sr/sr",
-  "ns/ns",
-  "kantetsu/kantetsu",
-  "kashima-rinkai/kashima-rinkai",
-  "hitachinaka/hitachinaka",
-];
+const generateScripts = (scripts, base = "") => {
+  for (const script of scripts) {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = `assets/${base}${script}.js`;
+    scriptElement.onload = () => {
+      const railwayStations = window[script.replace(/-|\/|\.|/g, "_")];
+      if (railwayStations) {
+        generateRailwayStations(script, railwayStations);
+      }
+    };
+    document.body.appendChild(scriptElement);
+  }
+};
 
-for (const script of kantoScripts) {
-  const scriptElement = document.createElement("script");
-  scriptElement.src = `assets/${script}.js`;
-  scriptElement.onload = () => {
-    const railwayStations = window[script.replace(/-|\/|\.|/g, "_")];
-    if (railwayStations) {
-      generateRailwayStations(script, railwayStations);
-    }
-  };
-  document.body.appendChild(scriptElement);
-}
+const mainScripts = ["kanto/kanto"];
+generateScripts(mainScripts);
+
 const appVersion = "1.0.0";
 
 document.getElementById("ver").textContent = "バージョン：" + appVersion;
-
-const CACHE_NAME = "station-picker-" + appVersion;
-const urlsToCache = [...kantoScripts.map((script) => `/assets/${script}.js`)];
-
-self.addEventListener("install", function (event) {
-  console.log("sw event: install called");
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", function (event) {
-  console.log("sw event: fetch called");
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response ? response : fetch(event.request);
-    })
-  );
-});
