df090d61b2b11441f3fae56ac5c5921f567950d8
wamo <ainznino@pm.me>
2025-10-06 22:13:28 +0900

    modify asa

--- assets/shikoku/asa/asa.js
+++ assets/shikoku/asa/asa.js
@@ -1,6 +1,8 @@
 const asaStations = [
+  { name: "阿波海南", id: "awa-kainan" },
   { name: "海部", id: "kaifu" },
   { name: "宍喰", id: "shishikui" },
+  { name: "甲浦", id: "kannoura" },
 ];
 
 generateStations(asaStations, "asa", "tokushima");
